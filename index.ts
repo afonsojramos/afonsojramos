c45c80cbe5ec2e97ac9aa357ef6134f0a4060890
Afonso Ramos <afonsojorgeramos@gmail.com>
2021-03-20 04:10:07 +0000

    fix fail on run

--- index.ts
+++ index.ts
@@ -6,72 +6,76 @@ import inquirer from 'inquirer';
 import clear from 'clear';
 import open from 'open';
 
-clear();
+function afonsojramos() {
+  clear();
 
-const prompt = inquirer.createPromptModule();
+  const prompt = inquirer.createPromptModule();
 
-const questions = [
-  {
-    type: 'list',
-    name: 'action',
-    message: 'What you want to do?',
-    choices: [
-      {
-        name: `Send me an ${chalk.green.bold('email')}?`,
-        value: () => {
-          open('mailto:afonsojorgeramos@gmail.com');
-          console.log('afonsojorgeramos@gmail.com is awaiting for your email!\nSee you soon.\n');
+  const questions = [
+    {
+      type: 'list',
+      name: 'action',
+      message: 'What you want to do?',
+      choices: [
+        {
+          name: `Send me an ${chalk.green.bold('email')}?`,
+          value: () => {
+            open('mailto:afonsojorgeramos@gmail.com');
+            console.log('afonsojorgeramos@gmail.com is awaiting for your email!\nSee you soon.\n');
+          },
         },
-      },
-      {
-        name: 'Just quit.',
-        value: () => {
-          console.log('Okay, I guess I can accept that, have a nice day!\n');
+        {
+          name: 'Just quit.',
+          value: () => {
+            console.log('Okay, I guess I can accept that, have a nice day!\n');
+          },
         },
-      },
-    ],
-  },
-];
+      ],
+    },
+  ];
 
-const data = {
-  name: chalk.bold.green('        Afonso Jorge Ramos'),
-  handle: chalk.white('@afonsojramos'),
-  work: `${chalk.white('Junior Software Developer @')} ${chalk.hex('#f25621').bold('Hostelworld')}`,
-  github: chalk.gray('https://github.com/') + chalk.green('afonsojramos'),
-  linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('afonsojramos'),
-  web: chalk.white('https://') + chalk.cyan('afonsojramos.me'),
-  npx: `${chalk.red('npx')} ${chalk.white('afonsojramos')}`,
+  const data = {
+    name: chalk.bold.green('        Afonso Jorge Ramos'),
+    handle: chalk.white('@afonsojramos'),
+    work: `${chalk.white('Junior Software Developer @')} ${chalk.hex('#f25621').bold('Hostelworld')}`,
+    github: chalk.gray('https://github.com/') + chalk.green('afonsojramos'),
+    linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('afonsojramos'),
+    web: chalk.white('https://') + chalk.cyan('afonsojramos.me'),
+    npx: `${chalk.red('npx')} ${chalk.white('afonsojramos')}`,
 
-  labelWork: chalk.white.bold('       Work:'),
-  labelGitHub: chalk.white.bold('     GitHub:'),
-  labelLinkedIn: chalk.white.bold('   LinkedIn:'),
-  labelWeb: chalk.white.bold('        Web:'),
-  labelCard: chalk.white.bold('       Card:'),
-};
+    labelWork: chalk.white.bold('       Work:'),
+    labelGitHub: chalk.white.bold('     GitHub:'),
+    labelLinkedIn: chalk.white.bold('   LinkedIn:'),
+    labelWeb: chalk.white.bold('        Web:'),
+    labelCard: chalk.white.bold('       Card:'),
+  };
 
-const me = boxen(
-  [
-    `${data.name} / ${data.handle}`,
-    ``,
-    `${data.labelWork}  ${data.work}`,
-    `${data.labelGitHub}  ${data.github}`,
-    `${data.labelLinkedIn}  ${data.linkedin}`,
-    `${data.labelWeb}  ${data.web}`,
-    ``,
-    `${data.labelCard}  ${data.npx}`,
-    ``,
-    `${chalk.italic('Avid enthusiast for open source software development.')}`,
-    `${chalk.italic('Open source perspective of life.')}`,
-  ].join('\n'),
-  {
-    margin: 1,
-    float: 'center',
-    padding: 1,
-    borderStyle: 'single',
-    borderColor: 'cyan',
-  },
-);
+  const me = boxen(
+    [
+      `${data.name} / ${data.handle}`,
+      ``,
+      `${data.labelWork}  ${data.work}`,
+      `${data.labelGitHub}  ${data.github}`,
+      `${data.labelLinkedIn}  ${data.linkedin}`,
+      `${data.labelWeb}  ${data.web}`,
+      ``,
+      `${data.labelCard}  ${data.npx}`,
+      ``,
+      `${chalk.italic('Avid enthusiast for open source software development.')}`,
+      `${chalk.italic('Open source perspective of life.')}`,
+    ].join('\n'),
+    {
+      margin: 1,
+      float: 'center',
+      padding: 1,
+      borderStyle: 'single',
+      borderColor: 'cyan',
+    },
+  );
 
-console.info(me);
+  console.info(me);
 
-prompt(questions).then((answer) => answer.action());
+  prompt(questions).then((answer) => answer.action());
+}
+
+afonsojramos();
